706566e077c31d0218db314d9236fb096b9f7a9a
jangr <38382335+rpggamesstudio@users.noreply.github.com>
2026-05-26 15:41:26 +0200

    Update FetchWeather.java

--- src/main/java/com/example/weather_app/FetchWeather.java
+++ src/main/java/com/example/weather_app/FetchWeather.java
@@ -1,5 +1,6 @@
 package com.example.weather_app;
 
+import java.io.FileInputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -11,17 +12,34 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.function.Consumer;
 
 public class FetchWeather {
+    private static final String API_KEY = loadApiKey();
+
+    private static String loadApiKey() {
+        Properties props = new Properties();
+
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            props.load(fis);
+            return props.getProperty("OPENWEATHER_API_KEY");
+        } catch (Exception e) {
+            throw new RuntimeException("Could not load API key.");
+        }
+    }
 
     // Added callbacks so the UI can react when data is ready or fails
     public void getWeather(String city, Consumer<JSONObject> onSuccess, Consumer<String> onError) {
         new Thread(() -> {
             try {
-                String apiKey = "566988eea4abeb70f1caec04663b205b".trim();
+                if (API_KEY == null || API_KEY.isBlank()) {
+                    throw new RuntimeException(
+                            "OPENWEATHER_API_KEY environment variable not set."
+                    );
+                }
                 String encodedCity = URLEncoder.encode(city, StandardCharsets.UTF_8);
-                String URL = "https://api.openweathermap.org/data/2.5/forecast?q=" + encodedCity + "&units=metric&appid=" + apiKey;
+                String URL = "https://api.openweathermap.org/data/2.5/forecast?q=" + encodedCity + "&units=metric&appid=" + API_KEY;
 
                 HttpClient client = HttpClient.newHttpClient();
                 HttpRequest request = HttpRequest.newBuilder().uri(URI.create(URL)).build();
@@ -43,9 +61,9 @@ public class FetchWeather {
     public List<String> getCitySuggestions(String query) {
         List<String> suggestions = new ArrayList<>();
         try {
-            String apiKey = "566988eea4abeb70f1caec04663b205b".trim();
+
             String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
-            String URL = "http://api.openweathermap.org/geo/1.0/direct?q=" + encodedQuery + "&limit=5&appid=" + apiKey;
+            String URL = "http://api.openweathermap.org/geo/1.0/direct?q=" + encodedQuery + "&limit=5&appid=" + API_KEY;
 
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create(URL)).build();
